578e0e086f662a67edf3794b79b888939e824799
T0shiro <loic.gardaire@gmail.com>
2016-09-23 15:20:32 +0200

    saucisse

--- src/mcga/brainfuck/brainfuck.java
+++ src/mcga/brainfuck/brainfuck.java
@@ -5,7 +5,7 @@ package mcga.brainfuck;
  */
 public class brainfuck {
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("azaaa");
 
     }
 }
