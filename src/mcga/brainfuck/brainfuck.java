ed53dfdedf3eb9ee1fc27ed61a02f8b76e910420
Malandril <canava.thomas@gmail.com>
2016-09-23 14:43:53 +0200

    lolol

---
+++ src/mcga/brainfuck/brainfuck.java
@@ -0,0 +1,10 @@
+package mcga.brainfuck;
+
+/**
+ * Created by user on 23/09/2016.
+ */
+public class brainfuck {
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
