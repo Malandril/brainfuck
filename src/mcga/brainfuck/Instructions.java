c308ec1ed4623d1e08837684a7fef28e9691bd23
Malandril <canava.thomas@gmail.com>
2016-09-28 15:13:45 +0200

    instuction enum not the best way but it works

---
+++ src/mcga/brainfuck/Instructions.java
@@ -0,0 +1,25 @@
+package mcga.brainfuck;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by user on 28/09/2016.
+ */
+public enum Instructions {
+    INCR("INCR", "+"),
+    DECR("DECR", "-"),
+    LEFT("LEFT", "<"),
+    RIGHT("RIGHT", ">");
+
+    List<String> names;
+
+    Instructions(String... names) {
+        this.names = Arrays.asList(names);
+    }
+
+    public Instructions hasInstruction(String str){
+        return Arrays.stream(Instructions.values()).filter(s->{return s.names.contains(str);}).findFirst().orElse(null);
+    }
+
+}
