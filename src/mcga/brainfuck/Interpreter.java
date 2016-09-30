f0a6e45b8ff2cc05a10f2e194f0b6f3c96c3ebdc
trinity357 <johann.mortara@gmail.com>
2016-09-30 19:10:43 +0200

    Plusieurs caractères sur une ligne

--- src/mcga/brainfuck/Interpreter.java
+++ src/mcga/brainfuck/Interpreter.java
@@ -1,5 +1,7 @@
 package mcga.brainfuck;
 
+import com.sun.org.apache.bcel.internal.generic.Instruction;
+
 import java.io.InputStream;
 import java.util.Scanner;
 
@@ -24,39 +26,54 @@ public class Interpreter {
 
         while (scanner.hasNext()) {
             str = scanner.nextLine();
-            Instructions instr = Instructions.hasInstruction(str);
-            isolCommand(str);
-            if (instr != null) {
-                switch (instr) {
-                    case INCR:
-                        new Operation().incrementation();
-                        break;
-                    case DECR:
-                        new Operation().decrementation();
-                        break;
-                    case LEFT:
-                        new Operation().moveL();
-                        break;
-                    case RIGHT:
-                        new Operation().moveR();
-                        break;
-                    default:
-                        System.out.println("looooooool");
-                        break;
-                }
+            if (readLine(str)) {
+                Instructions instr = Instructions.hasInstruction(str);
+                instrAction(instr);
+            } else {
+                isolCommand(str);
             }
         }
     }
 
-    public void isolCommand(String str){
-        // TODO: 28/09/2016 plusieurs instructions longues par ligne ? 
-        // TODO: 28/09/2016 INCRD -> INCR ou rien ?
-        // TODO: 28/09/2016 séparation par des espaces des commandes longues ?
-        String tmp = "";
-        for(int i=0 ; i<str.length() ; i++){
+    public void isolCommand(String str) {
+        for (int i = 0 ; i < str.length() ; i++) {
+            Instructions strInstr = Instructions.hasInstruction(Character.toString(str.charAt(i)));
+            instrAction(strInstr);
             System.out.println(str.charAt(i));
-            tmp += str.charAt(i);
         }
-        System.out.println(tmp);
+    }
+
+    /**
+     * Detects if a line of is a line of short or long instructions.
+     * A long instruction is composed of letters. We just need to test if the first character is a letter.
+     * @param line
+     * @return true if long instructions, false if short instructions
+     */
+    public boolean readLine(String line) {
+        char firstChar = line.charAt(0);
+        return (Character.getType(firstChar) == Character.UPPERCASE_LETTER);
+
+    }
+
+    public void instrAction(Instructions instr) {
+        if (instr != null) {
+            switch (instr) {
+                case INCR:
+                    new Operation().incrementation();
+                    break;
+                case DECR:
+                    new Operation().decrementation();
+                    break;
+                case LEFT:
+                    new Operation().moveL();
+                    break;
+                case RIGHT:
+                    new Operation().moveR();
+                    break;
+                default:
+                    System.out.println("looooooool");
+                    break;
+            }
+        }
     }
 }
