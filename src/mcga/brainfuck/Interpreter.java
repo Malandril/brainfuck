ee7926e99fe24abbf68dd4a88e492728e8de421b
Malandril <canava.thomas@gmail.com>
2016-10-12 16:14:24 +0200

    SLIDE 7

--- src/mcga/brainfuck/Interpreter.java
+++ src/mcga/brainfuck/Interpreter.java
@@ -15,7 +15,6 @@ public class Interpreter {
      * In case a file is specified in the launching command, this constructor is called.
      * @param stream Stream from file
      */
-    // TODO: 30/09/2016 changer ce commentaire
     public Interpreter(InputStream stream) {
         this.stream = stream;
     }
@@ -47,22 +46,6 @@ public class Interpreter {
 
 
     /**
-     * Detects if a str of is a str of short or long instructions.
-     * A long instruction is composed of uppercase letters.
-     * We just need to test if the first character is a letter.
-     * @param str Line to test
-     * @return true if long instructions, false if short instructions
-     */
-
-    public boolean isUppercase(String str) {
-        char firstChar = str.charAt(0);
-        return (Character.getType(firstChar) == Character.UPPERCASE_LETTER);
-    }
-
-
-
-
-    /**
      * Prints on the standard output the shortened representation of the program given as input.
      */
     public void rewriteFile() {
@@ -71,7 +54,7 @@ public class Interpreter {
         String strConverted = "";
         while (scanner.hasNext()) {
             str = scanner.nextLine();
-            if (isUppercase(str)) {
+            if (InstructionFactory.isLongSyntax(str)) {
                 strConverted += InstructionFactory.getShortSyntax(str);
             } else {
                 strConverted += str;
