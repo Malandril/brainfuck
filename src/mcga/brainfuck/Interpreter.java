638567fae683b07e59df5e8cbf9708cb5f3a60a2
T0shiro <loic.gardaire@gmail.com>
2016-11-02 17:48:27 +0100

    test boucle v0.1

--- src/mcga/brainfuck/Interpreter.java
+++ src/mcga/brainfuck/Interpreter.java
@@ -1,22 +1,30 @@
 package mcga.brainfuck;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This class extends Parser. It rewrites the methods from the superclass to interpret the parsed actions.
  */
+
 public class Interpreter extends Parser {
 
     /**
      * Default constructor of the class.
+     *
      * @see Parser#Parser()
      */
+    private static List<Instruction> instructions = new ArrayList<>();
+    public static boolean test=true;
+
     public Interpreter() {
         super();
     }
 
     /**
      * Constructor defining the stream in parameter as the input stream.
+     *
      * @param stream
      * @see Parser#Parser(InputStream)
      */
@@ -24,25 +32,31 @@ public class Interpreter extends Parser {
         super(stream);
     }
 
+    @Override
+    public void parseFile() {
+        super.parseFile();
+        for (int i = 0; i < instructions.size(); i++) {
+            try {
+                if(test) {
+                    instructions.get(i).interpret();
+                }
+            } catch (InvalidValueException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     /**
      * This method overrides Parser#execute called in Parser#parseFile so that it creates the Instruction
      * corresponding to the String in parameter and interprets it.
+     *
      * @param str String corresponding to the Instruction
      * @throws InvalidInstructionException
      * @see Parser#execute(String)
      */
     @Override
     public void execute(String str) throws InvalidInstructionException {
-        try {
-            InstructionFactory.createInstruction(str).interpret();
-        } catch (InvalidValueException e) {
-            System.err.println(e.getMessage());
-            System.exit(1);
-        } catch (IndexOutOfBoundsException e) {
-            System.err.println(e.getMessage());
-            System.exit(2);
-        }
+        instructions.add(InstructionFactory.createInstruction(str));
     }
 
 
