a741fe18c86680014b74fce1fe917764aa870e0b
Johann Mortara <johann.mortara@etu.unice.fr>
2017-01-04 17:08:40 +0100

    Création de InstructionCreatorTest et ProcedureTest

---
+++ src/test/java/mcga/brainfuck/ProcedureTest.java
@@ -0,0 +1,33 @@
+package mcga.brainfuck;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by user on 04/01/2017.
+ */
+public class ProcedureTest {
+
+    Procedure procedure;
+
+    @Before
+    public void setUp() throws Exception {
+
+
+    }
+
+    @Test
+    public void interpret() throws Exception {
+
+    }
+
+    @Test
+    public void free() throws Exception {
+
+    }
+
+}
