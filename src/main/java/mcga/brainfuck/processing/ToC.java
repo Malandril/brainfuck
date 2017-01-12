b619434513e7994d81a5e6736da467b81bc14d89
Thomas Canava <thomas.canava@etu.unice.fr>
2017-01-12 15:53:02 +0100

    FINAL FLAAAAASH

--- src/main/java/mcga/brainfuck/processing/ToC.java
+++ src/main/java/mcga/brainfuck/processing/ToC.java
@@ -111,11 +111,11 @@ public class ToC extends Parser {
             instructions.add(sb.toString());
             readText(code);
             if (function) {
-                instructions.add("int resPtr=ptr;");
+                instructions.add("int res=tab[ptr];");
             }
             instructions.add("ptr = end;\n" + "while (ptr >= start) {\n" + "   tab[ptr--] = 0;\n" + "}\n" + "start = tmpStart;\n" + "end = tmpEnd;\n" + "ptr = tmpPtr;\n");
             if (function) {
-                instructions.add("return tab[resPtr];");
+                instructions.add("return res;");
             }
             instructions.add("}");
             instructions = tmp;
